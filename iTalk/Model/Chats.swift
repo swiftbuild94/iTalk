979c9654d4162a0c48a47c62a1c7b70c1c03cdad
SwiftBuild <swiftbuild94@gmail.com>
2020-07-21 18:00:33 -0300

    Model

---
+++ iTalk/Model/Chats.swift
@@ -0,0 +1,28 @@
+//
+//  Chats.swift
+//  iTalk
+//
+//  Created by Patricio Benavente on 17/07/20.
+//  Copyright © 2020 Patricio Benavente. All rights reserved.
+//
+
+import Foundation
+
+struct Chats: Identifiable, Codable {
+	var id = UUID()
+	var typeOfContent: TypeOfContent?
+	var contentText: String?
+	var contentAudio: Data?
+	var contentVideo: Data?
+	var date = Date()
+	var delivered = false
+	var read = false
+	var readtime: Date?
+}
+
+enum TypeOfContent: String, Codable {
+	case text
+	case audio
+	case video
+	case live
+}
---
+++ iTalk/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  iTalk
+//
+//  Created by Patricio Benavente on 17/07/20.
+//  Copyright © 2020 Patricio Benavente. All rights reserved.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+	var id = UUID()
+	var name: String
+	var photo: String?
+	var phoneNumber: Int?
+	var email: String?
+	var chats: [Chats]?
+}
